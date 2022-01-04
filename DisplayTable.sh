a8087c2c8f904d9e1a347d5aff60fae01b99144a
alaafat7albab <alaa.fathalbab@gmail.com>
2022-01-04 05:29:48 +0200

    Display Table

---
+++ DisplayTable.sh
@@ -0,0 +1,12 @@
+echo "====================================="
+if [[ `ls ./databases/"$1"` == "" ]]
+then
+    clear
+    echo "There No Tables To Show"
+else
+    clear
+    echo "exsiting tables : "
+    echo `ls ./databases/"$1" `
+    echo "=================================="
+fi
+
