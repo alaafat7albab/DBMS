b356a6cec85367650e4108be6885f83d0226bb92
rehab-naguib <60330040+rehab-naguib@users.noreply.github.com>
2022-01-05 21:32:17 +0200

    Delete dbms directory

--- dbms/createDB.sh
+++
@@ -1,9 +0,0 @@
-read -p "Enter database name " dbName
-
-if [ -d ./databases/$dbName ]
-then
-	echo " this database already exist"
-else
-	mkdir ./databases/$dbName
-	echo "$dbName directory created"
-fi
--- dbms/listDB.sh
+++
@@ -1,8 +0,0 @@
-#to list directories only
-ls ./databases 2> err > output
-if [ $? -eq 0 ]
-then  
-	ls ./databases
-else
-echo "there no databases to show"
-fi
