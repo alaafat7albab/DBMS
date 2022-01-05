e4a325206ccc192ab9e303290740508f7cb3c629
alaafat7albab <alaa.fathalbab@gmail.com>
2022-01-06 00:18:42 +0200

    Display  Table

--- DisplayTable.sh
+++ DisplayTable.sh
@@ -1,12 +1,53 @@
-echo "====================================="
-if [[ `ls ./databases/"$1"` == "" ]]
-then
-    clear
-    echo "There No Tables To Show"
-else
-    clear
-    echo "exsiting tables : "
-    echo `ls ./databases/"$1" `
-    echo "=================================="
-fi
+#!/bin/bash
+. ./ShowTables.sh 
 
+
+displayTable(){
+    set +x 
+    while true
+    do    
+        echo "Please Enter Table Name"
+        read tableName
+        if [[ $tableName == / ]]
+        then 
+            showTable $dbName
+            echo "/ Is Not a Table Name"
+            displayTable
+        elif [[ $tableName == '' ]]
+        then 
+            showTables $dbName
+            echo "Table Name Can not be Empty"
+            displayTable
+        elif [[ ! -d ./databases/$dbName/$tableName ]]
+        then
+            showTable $dbName
+            echo "No Exiting Tables, Please Choose Another Name"
+            displayTable
+        else 
+            clear
+            echo "1 - Diplay Table $tableName Description"
+            echo "2 - Diplay Table $tableName Data"
+            read answer
+
+            case $answer in
+                1) 
+                    clear
+                    cat ./databases/$dbName/$tableName/$tableName"_"des 
+                    echo " "
+                    break
+                    ;;
+                2)
+                    clear 
+                    cat ./databases/$dbName/$tableName/$tableName"_"data
+                    echo " "
+                    break
+                    ;;
+                *)
+                    clear
+                    echo "it's not option"
+            esac
+        fi
+        break
+    done
+    set -x
+}
