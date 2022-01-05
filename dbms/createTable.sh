cfb701cf9339922db545c6a250e2ca726874ff00
rehab naguib <rehabnaguib8@gmail.com>
2022-01-05 21:30:41 +0200

    create table script

---
+++ dbms/createTable.sh
@@ -0,0 +1,55 @@
+shopt -s nocasematch
+echo "enter table name"
+read tableName
+if [ -f $tableName ]
+then 
+echo "this table is already exist"
+else 
+touch $tableName
+echo "enter number of columns"
+read columnNumber
+typeset -i index=1
+while [ $index -le $columnNumber ];
+do 
+	if [ $index -eq 1 ]
+	then
+	echo "consider coulmn number 1 is the primary key "
+	fi	
+	echo "column number $index name"
+	read coulmnName
+	echo "column datatype (int or string)"
+	select datatype in  "int" "string"
+	do
+	case $datatype in
+	"int" ) 
+	datatype="int"
+	break
+	;;
+	"string" )
+	datatype="string"
+	break;	
+	;;	
+	* )
+	echo "Invalid datatype";
+	read -p "Enter column datatype : [string/int]" datatype;
+	;;
+	esac	
+	done
+	echo $datatype
+#echo -n --> don't output the trailing newLine
+	echo -n $coulmnName":" >> $tableName;	
+	echo $index
+	if [ $index -eq $columnNumber ]
+	then
+	echo $datatype >> $tableName;
+	echo "hey there"
+	else
+	echo -n $datatype":" >> $tableName;
+	fi
+	index=$index+1
+done
+echo "from create table" >> $tableName
+echo "$tableName has been created"
+fi
+
+
