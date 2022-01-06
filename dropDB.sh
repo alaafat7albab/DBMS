bc47d5a9063a8fb2ffd7ac085af42ee20db79f85
alaafat7albab <alaa.fathalbab@gmail.com>
2022-01-06 20:54:56 +0200

    copy

---
+++ dropDB.sh
@@ -0,0 +1,9 @@
+read -p "Enter database name " dbName
+if [ -d ./databases/$dbName ]
+then
+        rm -r ./databases/$dbName
+	echo "$dbName database has been deleted"
+else
+        echo "there in no such a database"
+fi
+
